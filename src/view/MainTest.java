743faf19c1f9a25bd5a361559ca0378b6017f070
Alexandra Xerxenevsky <alexandra.jmx@gmail.com>
2022-06-26 14:30:19 -0300

    Novos destinatarios

--- src/view/MainTest.java
+++ src/view/MainTest.java
@@ -3,6 +3,7 @@ import controle.Processador;
 import model.Carta;
 import model.Correspondencia;
 import model.Destinatario;
+import model.Pacote;
 import model.dao.CorrespondenciaDAO;
 import model.dao.DestinatarioDAO;
 
@@ -14,6 +15,11 @@ public class MainTest {
         Destinatario nina = new Destinatario("132", "Nina", "32");
         nina.addAutorizado("133");
         destinatarioDAO.inserir(nina);
+        Destinatario carminha = new Destinatario("221", "Carminha", "21");
+        carminha.addAutorizado("222");
+        destinatarioDAO.inserir(carminha);
+        Destinatario agatha = new Destinatario("112", "Agatha", "12");
+        destinatarioDAO.inserir(agatha);
 
         Correspondencia correspondencia = new Carta(nina, true);
         correspondenciaDAO.inserir(correspondencia);
