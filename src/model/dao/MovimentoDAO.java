1e1c6329eb978a98c37193a27165282e07d45860
githiago-f <tfarias@protonmail.com>
2022-06-25 22:54:07 -0300

    Implementando busca por datas

--- src/model/dao/MovimentoDAO.java
+++ src/model/dao/MovimentoDAO.java
@@ -6,6 +6,7 @@ import model.Movimento;
 import view.errors.NaoEncontrado;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class MovimentoDAO implements OperacoesDAO {
@@ -49,5 +50,19 @@ public class MovimentoDAO implements OperacoesDAO {
 
     }
 
+    private boolean compareCalendars(Calendar calendar, Calendar calendar2) {
+        return calendar.get(Calendar.DATE) == calendar2.get(Calendar.DATE) &&
+        calendar.get(Calendar.MONTH) == calendar2.get(Calendar.MONTH) &&
+        calendar.get(Calendar.YEAR) == calendar2.get(Calendar.YEAR);
+    }
 
+    public List<Movimento> pesquisarPorData(Calendar calendar) {
+        List<Movimento> movimentos = new ArrayList<>();
+        for (Movimento movimento : movimentosList) {
+            if(compareCalendars(calendar, movimento.getData())) {
+                movimentos.add(movimento);
+            }
+        }
+        return movimentos;
+    }
 }
